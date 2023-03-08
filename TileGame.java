0b9ed639492655b76af0829e40ef3c4c4967c3c0
23NateK <n.kimber22@gmail.com>
2023-03-08 12:13:35 -0600

    Started making the board

--- TileGame.java
+++ TileGame.java
@@ -1,6 +1,8 @@
 import javax.swing.*;  
+import java.awt.event.*;
+import java.util.Arrays;
 public class TileGame{
-    public static void main(String[] args) {  
+    public static void Example() {  
         JFrame f=new JFrame();//creating instance of JFrame  
                 
         JButton b=new JButton("click");//creating instance of JButton  
@@ -12,4 +14,45 @@ public class TileGame{
         f.setLayout(null);//using no layout managers  
         f.setVisible(true);//making the frame visible  
     } 
+    public static void Game(){
+        int round=4;
+        int[][] board=new int[5][6];
+        for(int x=0;x<board.length;x++){
+            for(int y=0;y<board[0].length;y++){
+                board[x][y]=0;
+            }
+        }
+    for(int r=0;r<round;r++){
+        int x=(int) Math.round(Math.random()*5);
+        int y=(int) Math.round(Math.random()*6);
+        System.out.println(x);
+        System.out.println(y);
+        board[x][y]=r;
+    }
+
+        System.out.println(Arrays.deepToString(board));
+    }
+    public static void Window() {
+        JFrame f=new JFrame("Tile Game");
+        JButton start=new JButton("Start");
+        start.addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) { 
+                Game();
+            } 
+        });
+        start.setBounds(0,0,100,50);
+        f.add(start);
+
+
+
+
+
+        f.setSize(1000,500); 
+        f.setLayout(null); 
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Window();
+    }
 }
